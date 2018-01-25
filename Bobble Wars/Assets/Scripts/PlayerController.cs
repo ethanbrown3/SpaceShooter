788021144eeecfc62896562aa4440ccfc52a1eab
Ethan Brown <ethanbrown3@mail.weber.edu>
2018-01-24 21:31:46 -0700

    Marine's head is now bobbing

--- Bobble Wars/Assets/Scripts/PlayerController.cs
+++ Bobble Wars/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	public float moveSpeed = 50.0f;
+	public Rigidbody head;
 	private CharacterController characterController;
 
 	void Start () {
@@ -14,5 +15,16 @@ public class PlayerController : MonoBehaviour {
 	void Update () {
 		Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 		characterController.SimpleMove(moveDirection * moveSpeed);
+	}
+
+	// Gaurenteed to be called at consistent intervals regardless of framerate
+	// Used for physics
+	void FixedUpdate() {
+		Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		if (moveDirection == Vector3.zero) {
+			// TODO
+		} else {
+			head.AddForce(transform.right * 150, ForceMode.Acceleration);
+		}
 	}
 }
