41da0398bc1a88f22e9ebf1e05aba5ef614fd7ed
Ethan Brown <ethanbrown3@mail.weber.edu>
2018-02-02 13:03:42 -0700

    WallTrigger is working for lowering the walls when the player enters it.

---
+++ Assets/_Scripts/ArenaWall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArenaWall : MonoBehaviour {
+    private Animator arenaAnimator;
+
+	// Use this for initialization
+	void Start () {
+        GameObject arena = transform.parent.gameObject;
+        arenaAnimator = arena.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter () {
+        arenaAnimator.SetBool("IsLowered", true);
+    }
+
+    private void OnTriggerExit () {
+        arenaAnimator.SetBool("IsLowered", false);
+    }
+}
